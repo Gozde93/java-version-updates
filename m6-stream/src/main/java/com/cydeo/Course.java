8c8f8b484ead2434f01c802ce611a4b89d9d58f6
gozdediri <dirigozde@gmail.com>
2024-02-26 15:44:50 -0600

    Stream

---
+++ m6-stream/src/main/java/com/cydeo/Course.java
@@ -0,0 +1,13 @@
+package com.cydeo;
+
+public class Course {
+
+    private String courseName;
+    private int courseId;
+
+
+    public Course(String courseName, int courseId) {
+        this.courseName = courseName;
+        this.courseId = courseId;
+    }
+}
---
+++ m6-stream/src/main/java/com/cydeo/Main.java
@@ -0,0 +1,36 @@
+package com.cydeo;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //Creating Stream from Array
+
+        String[] courses = {"Java", "Spring", "Agile"};
+
+        Stream<String> courseStream = Arrays.stream(courses);
+
+
+
+        //Creating Stream from Collection
+        List<String> courseList = Arrays.asList("Java", "Spring", "Agile");
+        Stream<String> courseList2 = courseList.stream();
+
+        List<Course> myCourses = Arrays.asList(
+                new Course("Java",100),
+                new Course("DS",101),
+                new Course("MS",102)
+        );
+
+        Stream<Course> myCourseStream = myCourses.stream();
+
+
+
+
+
+    }
+}
