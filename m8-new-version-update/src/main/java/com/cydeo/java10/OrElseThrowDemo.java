77efc210777054be1804750f07c2ce3de43bdd5e
gozdediri <dirigozde@gmail.com>
2024-03-05 19:58:21 -0600

    Java10

---
+++ m8-new-version-update/src/main/java/com/cydeo/java10/OrElseThrowDemo.java
@@ -0,0 +1,22 @@
+package com.cydeo.java10;
+
+import java.util.Optional;
+
+public class OrElseThrowDemo {
+
+    public static void main(String[] args) {
+
+        Optional<String> str = Optional.empty();
+        System.out.println(str.get());
+
+        if (str.isPresent()){
+            System.out.println(str.get());
+        }else {
+            //exception handling
+        }
+        System.out.println(str.orElseThrow());
+
+
+
+    }
+}
