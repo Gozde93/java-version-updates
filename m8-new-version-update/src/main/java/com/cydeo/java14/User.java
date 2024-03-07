d070b378e74409051b43bfec3a436a145f1bded5
gozdediri <dirigozde@gmail.com>
2024-03-07 14:12:40 -0600

    Java14

---
+++ m8-new-version-update/src/main/java/com/cydeo/java14/User.java
@@ -0,0 +1,16 @@
+package com.cydeo.java14;
+
+public record User(String name, String address) {
+
+    //final bu o yuzden setter yok
+
+    //ALL ARG CONST
+    //GETTER
+    //TO STRING
+    //EQUALS
+    //HASH CODE
+
+
+
+
+}
